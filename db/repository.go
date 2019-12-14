37e10934ef516d67e4fff0eacdcc83731ccb327b
markel3266 <markel3266@gmail.com>
2019-12-14 17:51:32 +0200

    added nginx config files and added lb service to docker-compose

--- db/repository.go
+++
@@ -1,8 +0,0 @@
-package db
-
-// Repository define i/o methods for MongoDB
-type Repository interface {
-	GetTransactionByID(transactionID string) (Transaction, error)
-	GetAllTransactions(accountID string) ([]Transaction, error)
-	SaveOneTransaction(transaction *Transaction) error
-}
--- db/transaction.go
+++
@@ -1,9 +0,0 @@
-package db
-
-import "github.com/lungria/mono"
-
-// Transaction ...
-type Transaction struct {
-	AccountId     string             `json:"account" bson:"account_id"`
-	StatementItem mono.StatementItem `json:"statementItem" bson:"statement_item"`
-}
