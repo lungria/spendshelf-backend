1b45b3d2c0fb2d4c847eb3b8e29ca4c33a1c4d4a
markel3266 <markel3266@gmail.com>
2019-12-18 21:17:09 +0200

    moved main.go to cmd dir

--- src/pkg/webhook/cmd/webhook/main.go
+++ src/pkg/webhook/cmd/webhook/main.go
@@ -4,38 +4,43 @@ import (
 	"context"
 	"flag"
 	"github.com/lungria/spendshelf-backend/src/pkg/webhook"
-	"log"
+	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 	"time"
 )
 
 func main() {
 	var err error
+
 	addr := flag.String("addr", ":8080", "HTTP address of server")
 	flag.Parse()
 
-	s := webhook.NewServer(*addr, "", "")
+	s := webhook.NewServer(*addr, "SpendShelf", "mongodb://root:toor@localhost:27017")
 
+	done := make(chan bool, 1)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
 
 	go func() {
-		if err = s.HTTPServer.ListenAndServe(); err != nil {
-			log.Fatalf("ListenAndServe failed %v", err)
+		<-sigChan
+		s.Logger.Info("Server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		s.HTTPServer.SetKeepAlivesEnabled(false)
+		if err = s.HTTPServer.Shutdown(ctx); err != nil {
+			s.Logger.Fatalf("Couldn't gracefully shutdown the server: %v\n", err)
 		}
+		close(done)
 	}()
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	<-sigChan
-
-	log.Println("Shutdown server...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err = s.HTTPServer.Shutdown(ctx); err != nil {
-		log.Fatalln("Server shutdown: ", err)
+	if err = s.HTTPServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		s.Logger.Fatalf("Couldn't listen on %v: %v\n", &addr, err)
 	}
 
-	log.Println("Server exiting")
+	<- done
+	s.Logger.Info("Server stopped")
+
 }
