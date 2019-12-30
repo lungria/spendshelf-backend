68f59b4ad01800140dc6a2dcfd12be1831b3f8cd
markel3266 <markel3266@gmail.com>
2019-12-30 13:31:27 +0200

    Added comment for Transactions struct

--- src/models/transactions.go
+++ src/models/transactions.go
@@ -2,7 +2,7 @@ package models
 
 import "github.com/lungria/mono"
 
-// Transaction ...
+// Transaction struct using in response from mono API and model in DB
 type Transaction struct {
 	AccountId     string             `json:"account" bson:"account_id"`
 	StatementItem mono.StatementItem `json:"statementItem" bson:"statement_item"`
