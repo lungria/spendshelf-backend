f0a3bffc9aef84704b9137df4072a12b86c0b22b
Artem <kolomytsev1996@gmail.com>
2020-01-10 01:12:11 +0200

    update transaction json model

--- src/models/transaction.go
+++ src/models/transaction.go
@@ -4,10 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Transaction represents a model of transactions in database
 type Transaction struct {
-	ID          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-	Time        primitive.DateTime `json:"time" bson:"time"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Time        primitive.DateTime `json:"dateTime" bson:"time"`
 	Description string             `json:"description" bson:"description"`
-	CategoryID  primitive.ObjectID `json:"category_id" bson:"category_id,omitempty"`
+	CategoryID  primitive.ObjectID `json:"categoryId,omitempty" bson:"category_id,omitempty"`
 	Amount      int64              `json:"amount" bson:"amount"`
 	Balance     int64              `json:"balance" bson:"balance"`
 }
