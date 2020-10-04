eeba407126ddb122a5944a181ed72886aa25750f
SuddenGunter <kolomytsev1996@gmail.com>
2020-10-04 20:07:39 +0300

    add schema

---
+++ schema/category.sql
@@ -0,0 +1,5 @@
+CREATE TABLE category (
+    ID int PRIMARY KEY,
+    createdAt timestamp NOT NULL,
+    name varchar(50) NOT NULL,
+);
---
+++ schema/transaction.sql
@@ -0,0 +1,9 @@
+CREATE TABLE transaction (
+    ID varchar(50) PRIMARY KEY,
+    time timestamp NOT NULL,
+    description varchar(50) NOT NULL,
+    mcc integer NOT NULL,
+    hold boolean NOT NULL,
+    amount bigint NOT NULL,
+    accountID varchar(50) NOT NULL
+);
