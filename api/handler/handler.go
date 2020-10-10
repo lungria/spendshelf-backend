80c78f5e5883a8cf81a3d04e1efb239f5e7cb625
SuddenGunter <kolomytsev1996@gmail.com>
2020-10-10 16:19:26 +0300

    add stub for transaction handler

---
+++ api/handler/handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type TransactionHandler struct {
+
+}
+
+f
+
+func (t TransactionHandler) BindRoutes(engine *gin.Engine) {
+	panic("implement me")
+}
+
