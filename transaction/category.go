03f639b922e08083948f25f4cc01da43bf4c369e
SuddenGunter <kolomytsev1996@gmail.com>
2020-10-10 16:47:37 +0300

    fix comment for default category

--- transaction/category.go
+++ transaction/category.go
@@ -1,4 +1,4 @@
 package transaction
 
-// DefaultCategoryID is the ID of category, that must be used for all new imported transaction.
+// DefaultCategoryID is the ID of category, that must be used for all new imported transactions.
 const DefaultCategoryID = 1
