d395be6a3be6121bc4086f8a8ca490ec5b64581f
SuddenGunter <kolomytsev1996@gmail.com>
2020-10-29 20:59:07 +0200

    use quoted identifiers

--- storage/schema/category.sql
+++ storage/schema/category.sql
@@ -1,6 +1,6 @@
 CREATE TABLE category (
-    ID integer PRIMARY KEY,
-    createdAt timestamp NOT NULL,
-    name varchar(25) NOT NULL,
-    logo varchar(30) NOT NULL
+    "ID" integer PRIMARY KEY,
+    "createdAt" timestamp NOT NULL,
+    "name" varchar(25) NOT NULL,
+    "logo" varchar(30) NOT NULL
 );
--- storage/schema/transaction.sql
+++ storage/schema/transaction.sql
@@ -1,13 +1,11 @@
 CREATE TABLE transaction (
-    ID varchar(50) PRIMARY KEY,
-    time timestamp NOT NULL,
-    description varchar(50) NOT NULL,
-    mcc integer NOT NULL,
-    hold boolean NOT NULL,
-    amount bigint NOT NULL,
-    accountID varchar(50) NOT NULL,
-    categoryID integer references category(ID) ON DELETE RESTRICT NOT NULL,
-    lastUpdatedAt timestamp NOT NULL
+    "ID" varchar(50) PRIMARY KEY,
+    "time" timestamp NOT NULL,
+    "description" varchar(50) NOT NULL,
+    "mcc" integer NOT NULL,
+    "hold" boolean NOT NULL,
+    "amount" bigint NOT NULL,
+    "accountID" varchar(50) NOT NULL,
+    "categoryID" integer references category("ID") ON DELETE RESTRICT NOT NULL,
+    "lastUpdatedAt" timestamp NOT NULL
 );
-
-
