21d73b25c3545d4532bed0bc3ca7ebef54ce34c8
SuddenGunter <kolomytsev1996@gmail.com>
2020-11-09 22:34:39 +0200

    db schema: add accounts table

--- storage/schema/migrations/1603954740_init.sql
+++ storage/schema/migrations/1603954740_init.sql
@@ -1,3 +1,10 @@
+CREATE TABLE category (
+    "ID" integer PRIMARY KEY,
+    "createdAt" timestamp NOT NULL,
+    "name" varchar(25) NOT NULL,
+    "logo" varchar(30) NOT NULL
+);
+
 CREATE TABLE transaction (
     "ID" varchar(50) PRIMARY KEY,
     "time" timestamp NOT NULL,
@@ -5,7 +12,7 @@ CREATE TABLE transaction (
     "mcc" integer NOT NULL,
     "hold" boolean NOT NULL,
     "amount" bigint NOT NULL,
-    "accountID" varchar(50) NOT NULL references account("ID") ON DELETE RESTRICT NOT NULL,
+    "accountID" varchar(50) NOT NULL,
     "categoryID" integer references category("ID") ON DELETE RESTRICT NOT NULL,
     "lastUpdatedAt" timestamp NOT NULL
 );
