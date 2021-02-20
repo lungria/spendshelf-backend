fe777447d44b9f8bb82b31c6d5798981b5c6e929
SuddenGunter <kolomytsev1996@gmail.com>
2021-02-20 22:49:11 +0200

    fix typo: 0o1 -> 1

--- storage/transaction_test.go
+++ storage/transaction_test.go
@@ -330,7 +330,7 @@ func TestGetReport_WithProductionSchema_NoErrorReturned(t *testing.T) {
 
 	report, err := db.GetReport(
 		context.Background(),
-		time.Date(2020, 10, 0o1, 0, 0, 0, 0, time.UTC),
+		time.Date(2020, 10, 1, 0, 0, 0, 0, time.UTC),
 		time.Date(2020, 10, 12, 0, 0, 0, 0, time.UTC))
 
 	assert.NoError(t, err)
