c5ce00403398a185bae857e95f52a81152e62343
SuddenGunter <kolomytsev1996@gmail.com>
2021-10-30 10:34:02 +0300

    fix indexes

--- storage/schema/migrations/1635577529_add_indexes.sql
+++ storage/schema/migrations/1635577529_add_indexes.sql
@@ -1,13 +1,18 @@
 BEGIN;
 
-CREATE INDEX accountID_time_idx on transaction("accountID", "time")
+CREATE INDEX accountID_time_idx on transaction("accountID", "time");
 
-CREATE INDEX time_categoryID_idx ON transaction ("time", "categoryID");
+CREATE INDEX time_categoryID_idx ON transaction("time", "categoryID");
 
-CREATE INDEX startsAt_idx ON budget ("startsAt");
+CREATE INDEX startsAt_idx ON budget("startsAt");
 
-CREATE INDEX createdAt_idx ON account ("createdAt");
+CREATE INDEX createdAt_idx ON account("createdAt");
 
-CREATE INDEX budgetID_amount_idx ON "limit" ("budgetID_amount_idx");
+CREATE INDEX budgetID_amount_idx ON "limit"("budgetID", "amount");
 
 COMMIT;
+
+REINDEX TABLE transaction;
+REINDEX TABLE budget;
+REINDEX TABLE account;
+REINDEX table "limit";
